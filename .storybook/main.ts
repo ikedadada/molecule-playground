e264c24769b7cce787a95df5bab6867e23be99bc
koichi.ikeda <koichiikeda.0111@gmail.com>
2025-06-08 12:07:45 +0900

    feat:storybookの設定を追加

---
+++ .storybook/main.ts
@@ -0,0 +1,21 @@
+import type { StorybookConfig } from '@storybook/react-vite';
+
+const config: StorybookConfig = {
+    stories: ['../app/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+    addons: [
+        '@storybook/addon-onboarding',
+        '@chromatic-com/storybook',
+        '@storybook/addon-docs',
+        '@storybook/addon-a11y',
+        '@storybook/addon-vitest'
+    ],
+    framework: {
+        name: '@storybook/react-vite',
+        options: {
+            builder: {
+                viteConfigPath: 'vite.storybook.config.ts'
+            }
+        }
+    }
+};
+export default config;
