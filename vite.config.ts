de394705103e3689e65f88d5a61b6277d4fddc03
koichi.ikeda <koichiikeda.0111@gmail.com>
2025-06-08 14:58:17 +0900

    fix:GitHub Pages側で他ファイルが読めていないので、basePathの処理を追加

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,10 @@ import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
+const repo = 'molecule_playground';
+const base = process.env.GITHUB_ACTIONS === 'true' ? `/${repo}/` : '/';
+
 export default defineConfig({
-    plugins: [tailwindcss(), reactRouter(), tsconfigPaths()]
+    plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+    base
 });
